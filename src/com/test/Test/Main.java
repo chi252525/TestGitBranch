3580519bfdc42864d45806fbbcec99ba09fb6ff9
ning yeh <ningyeh@va7.com.tw>
2019-12-24 17:07:03 +0800

    second commit

--- src/com/test/Test/Main.java
+++ src/com/test/Test/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("這是Master");
-
+		System.out.println("Ver2新加的1");
+		System.out.println("Ver2新加的2");
+		System.out.println("Ver2新加的3");
+		System.out.println("Ver2新加的4");
+		
 	}
 
 }
